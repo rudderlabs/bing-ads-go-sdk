3c5540784a3c8c64c4948e8c35b391743170ac4e
Dilip Kola <kdilipkola@gmail.com>
2023-04-19 22:03:20 +0530

    refactor: oauth2 generate token function

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
+
 	"os"
 	"strings"
 
@@ -15,37 +15,7 @@ import (
 	"golang.org/x/oauth2"
 )
 
-func generate_token(params url.Values) (*oauth2.Token, error) {
-	endpoint := "https://login.microsoftonline.com/common/oauth2/v2.0/token"
-	method := "POST"
-	payload := strings.NewReader(params.Encode())
-	client := &http.Client{}
-	req, err := http.NewRequest(method, endpoint, payload)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	res, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	var tokenResponse oauth2.Token
-	if err := json.Unmarshal(body, &tokenResponse); err != nil {
-		return nil, err
-	}
-	if tokenResponse.RefreshToken == "" {
-		return nil, fmt.Errorf("invalid token")
-	}
-	return &tokenResponse, nil
-}
-
-func generate_refresh_token() (*oauth2.Token, error) {
+func generate_refresh_token(config *oauth2.Config) (*oauth2.Token, error) {
 	token, err := os.ReadFile("token.json")
 	if err == nil && len(token) > 0 {
 		var tokenResponse oauth2.Token
@@ -55,14 +25,7 @@ func generate_refresh_token() (*oauth2.Token, error) {
 		return &tokenResponse, nil
 	}
 
-	params := url.Values{}
-	params.Add("client_id", os.Getenv("CLIENT_ID"))
-	params.Add("scope", os.Getenv("SCOPE"))
-	params.Add("code", os.Getenv("CODE"))
-	params.Add("redirect_uri", os.Getenv("REDIRECT_URI"))
-	params.Add("grant_type", "authorization_code")
-	params.Add("client_secret", os.Getenv("CLIENT_SECRET"))
-	tokenResponse, err := generate_token(params)
+	tokenResponse, err := config.Exchange(context.TODO(), os.Getenv("CODE"))
 	if err != nil {
 		return nil, err
 	}
@@ -71,34 +34,18 @@ func generate_refresh_token() (*oauth2.Token, error) {
 	return tokenResponse, nil
 }
 
-func generate_access_token() (*oauth2.Token, error) {
-	refreshToken, err := generate_refresh_token()
+func generate_access_token(config *oauth2.Config) (*oauth2.Token, error) {
+	refreshToken, err := generate_refresh_token(config)
 	if err != nil {
 		return nil, err
 	}
 
-	params := url.Values{}
-	params.Add("client_id", os.Getenv("CLIENT_ID"))
-	params.Add("scope", os.Getenv("SCOPE"))
-	params.Add("refresh_token", refreshToken.RefreshToken)
-	params.Add("redirect_uri", os.Getenv("REDIRECT_URI"))
-	params.Add("client_secret", os.Getenv("CLIENT_SECRET"))
-	params.Add("grant_type", "refresh_token")
-	return generate_token(params)
+	return config.TokenSource(context.TODO(), refreshToken).Token()
 }
 
-func get_session_config(token *oauth2.Token) bingads.SessionConfig {
+func get_session_config(config *oauth2.Config, token *oauth2.Token) bingads.SessionConfig {
 	return bingads.SessionConfig{
-		OAuth2Config: &oauth2.Config{
-			ClientID:     os.Getenv("CLIENT_ID"),
-			ClientSecret: os.Getenv("CLIENT_SECRET"),
-			Endpoint: oauth2.Endpoint{
-				AuthURL:  bingads.AuthEndpoint,
-				TokenURL: bingads.TokenEndpoint,
-			},
-			Scopes:      strings.Split(os.Getenv("SCOPE"), " "),
-			RedirectURL: "https://login.microsoftonline.com/common/oauth2/nativeclient",
-		},
+		OAuth2Config:  config,
 		OAuth2Token:    token,
 		AccountId:      os.Getenv("CUSTOMER_ACCOUNT_ID"),
 		CustomerId:     os.Getenv("CUSTOMER_ID"),
@@ -113,11 +60,23 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	token, err := generate_access_token()
+	oauth2Config := &oauth2.Config{
+		ClientID:     os.Getenv("CLIENT_ID"),
+		ClientSecret: os.Getenv("CLIENT_SECRET"),
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  bingads.AuthEndpoint,
+			TokenURL: bingads.TokenEndpoint,
+		},
+		Scopes:      strings.Split(os.Getenv("SCOPE"), " "),
+		RedirectURL: os.Getenv("REDIRECT_URI"),
+	}
+	fmt.Println(oauth2Config.AuthCodeURL("state"))
+
+	token, err := generate_access_token(oauth2Config)
 	if err != nil {
 		log.Fatal(err)
 	}
-	session := bingads.NewSession(get_session_config(token))
+	session := bingads.NewSession(get_session_config(oauth2Config, token))
 	service := bingads.NewBulkService(session)
 	resp, err := service.GetBulkUploadUrl()
 	if err != nil {
